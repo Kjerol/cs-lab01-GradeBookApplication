7468f3e4ad5053b01bf192424158927f315b16c8
Kjerol <thearon8382@gmail.com>
2022-04-03 21:28:45 +0200

    CalculateStudentStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -26,4 +26,37 @@ namespace GradeBook.GradeBooks
             if (ranking <= .6) return 'C';
             if (ranking <= .8) return 'D';
             else return 'F';
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
+        }
+
+
+
+
+
+
+
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStudentStatistics(name);
+
+
+
+
+
+
+
+
+
+
+
         } } }
